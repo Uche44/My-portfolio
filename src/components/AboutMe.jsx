7cade2c46c5fb9d3a37e0bac2c2ed0dd125f7a72
Uche44 <perpetualuchechukwu808@gmail.com>
2025-03-12 18:07:47 +0100

    commit

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,14 @@
 const AboutMe=()=>{
     return(
-        <section className='bg-green'></section>
+        <section className='bg-green border-2 border-white'>
+            <h2 className='text-white'>About Me</h2>
+            <div className=''>
+                <p className='text-white'>
+
+Hi! I'm Perpetual Asogwa, a passionate Frontend Developer who loves turning ideas into interactive, user-friendly experiences. With expertise in React, JavaScript, and modern UI frameworks, I build sleek, responsive web applications that blend aesthetics with functionality. I’m always exploring new technologies, optimizing performance, and pushing the boundaries of web development. Let’s create something amazing! 
+</p>
+            </div>
+        </section>
     )
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import HeroSection from "./Hero";
+import AboutMe from './AboutMe'
 const HomePage = () => {
   return (
     <div className="px-3 md:px-0 bg-black">
       <Header />
       <HeroSection />
+      <AboutMe/>
     </div>
   );
 };
